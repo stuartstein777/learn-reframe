cb65d9567afd4cdafb6a05a3204e7b1587451ac8
Stuart Stein <qmstuart@gmail.com>
2021-03-11 18:00:41 +0000

    Messing, added nrepl port to shadow-cljs.edn

--- src/main/exfn/app.cljs
+++ src/main/exfn/app.cljs
@@ -2,17 +2,24 @@
   (:require [reagent.dom :as r]))
 
 (comment
-  (str "To switch to cljs repl")
-  (shadow/repl :your-build-id))
+  (str "To switch to cljs repl evaluate this, don't use cljs drop down in repl tab.")
+  (shadow/repl :app)
+  (js/alert "hello!"))
+
+(defn random-string []
+  (->> "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+       (shuffle)
+       (take 5)
+       (apply str)))
 
 (defn app
   []
   [:div
-   [:h1 "Lets learn reagent and re-frame!"]
-   [:p "This is a paragraph"]
-   [:ul
+   [:h1 "Lets learn reagent and re-frame!!!"]
+   [:p {:class "bigp"} "This is a paragraph"]
+   [:ol
     (for [x (range 1 11)]
-      [:li (str "List item: " x)])]])
+      [:li {:key (str "li-item-" x)} (str (random-string))])]])
 
 (defn ^:dev/after-load start
   []
@@ -22,4 +29,3 @@
 (defn ^:export init []
   (js/console.log "Lets learn re-frame!")
   (start))
-
